e554bb4a0d9e72a456e6ccd9569198522c020a21
GluKosAlex <glukosalex@gmail.com>
2023-12-24 20:38:13 +0700

    feat(api): add custom error func and other utills

---
+++ utils/constants.js
@@ -0,0 +1,5 @@
+const ERROR_CODE_DUPLICATE_MONGO = 11000;
+const SALT_ROUNDS = 10;
+const URLExpression = /https?:\/\/(www\.)?[-a-zA-Z0-9._~:/?#[\]@!$&'()*+,;=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/i;
+
+export { ERROR_CODE_DUPLICATE_MONGO, SALT_ROUNDS, URLExpression };
